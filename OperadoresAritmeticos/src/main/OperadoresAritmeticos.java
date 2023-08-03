53bc34599b662abd404a479a130383de7ee96eae
nahuelgrijalba <nahuelgrijalba@nahuelgrijalba>
2023-08-03 17:41:35 -0300

    add operadoresAritmeticos

---
+++ OperadoresAritmeticos/src/main/OperadoresAritmeticos.java
@@ -0,0 +1,44 @@
+package main;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// Operadores aritmeticos
+		
+		int variableX = 50, variableY = 10;
+		int resultado;
+		
+		resultado = variableX + variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX - variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX * variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX / variableY;
+		System.out.println(resultado);
+		
+		variableX++; //incrementa en uno
+		System.out.println(variableX);
+		
+		variableX--; //decrece en uno
+		System.out.println(variableX);
+		
+		variableX += 100; //incrementa el valor de variableX en 100 (variableX = variableX + 100)
+		System.out.println(variableX);
+		
+		variableX -= 100; // el valor de variableX en 100 (variableX = variableX - 100)
+		System.out.println(variableX);
+		
+		variableX *= 100; // le asigna a variableX el valor de variableX por 3 (variableX = variableX * 3)
+		System.out.println(variableX);
+		
+		variableX /= 100; // le asigna a variableX el valor de variableX entre 3 (variableX = variableX / 3)
+		System.out.println(variableX);
+		
+
+	}
+
+}
---
+++ OperadoresAritmeticos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nahuelgrijalba
+ *
+ */
+module OperadoresAritmeticos {
+}
