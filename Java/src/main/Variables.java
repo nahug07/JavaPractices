45cd4020ad4119b8f2e781eb644fc0fe4f6c6292
nahuelgrijalba <nahuelgrijalba@gmail.com>
2023-08-03 17:17:16 -0300

    first commit

---
+++ Java/src/main/Variables.java
@@ -0,0 +1,40 @@
+package main;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 4 tipos de datos
+		 * 
+		 * Enteros  --> int
+		 * Decimales
+		 * Caracteres
+		 * Booleanos
+		 * */
+		
+		//ejecución del programa de forma descendente
+		
+		int version; //declaro la variable
+		version = 9; //le asigno un valor
+		version = 10; //puedo asignarle un nuevo valor
+		
+		int versionJava = 10; // declaro la variable asignandole un valor
+		
+		float versionEspecifica = 9.8f; //los flotantes deben terminar con el subfijo f
+		
+		char letra = 'a';
+		
+		boolean valor = true;
+		
+		int var1 = 1, var2 = 2, var3 = 3; //declaracion de multiples variables (siempre que sean del mismo tipo)
+		float pi = 3.1415f, e = 0.1010f; 
+		
+		final int constante1 = 15; //utilizando la palabra "final" declaramos una constante que no puede cambiar su valor, solo ser consultada
+		
+		System.out.println(version);
+
+	}
+
+}
