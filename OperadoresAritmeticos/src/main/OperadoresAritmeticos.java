e33288f61fab2936e337488bc8c0f7fd1d3382d0
nahuelgrijalba <nahuelgrijalba@nahuelgrijalba>
2023-08-03 17:58:38 -0300

    add cast

--- OperadoresAritmeticos/src/main/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/main/OperadoresAritmeticos.java
@@ -38,6 +38,16 @@ public class OperadoresAritmeticos {
 		variableX /= 100; // le asigna a variableX el valor de variableX entre 3 (variableX = variableX / 3)
 		System.out.println(variableX);
 		
+		//Cast
+		int variableA = 50;
+		float variableB = 5.5f;
+		
+		int resultado2 = variableA + (int)variableB; //convertimos el valor flotante en un entero eliminando el punto decimal
+		System.out.println(resultado2);
+		
+		float resultado3 = (float)variableA + variableB; //convertimos el valor entero en un foltante 
+		System.out.println(resultado3);
+		
 
 	}
 
