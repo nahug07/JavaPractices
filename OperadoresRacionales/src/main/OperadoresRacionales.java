53c04043e01410b186b9562aaf173cc98c595bf5
nahuelgrijalba <nahuelgrijalba@nahuelgrijalba>
2023-08-03 18:12:10 -0300

    add operadoresRacionales

---
+++ OperadoresRacionales/src/main/OperadoresRacionales.java
@@ -0,0 +1,32 @@
+package main;
+
+public class OperadoresRacionales {
+
+	public static void main(String[] args) {
+		// Operadores Racionales
+		
+		int variable1 = 300, variable2 = 200;
+		boolean resultado;
+		
+		
+		resultado = variable1 > variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 < variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 >= variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 <= variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 == variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 != variable2;
+		System.out.println(resultado);
+
+	}
+
+}
